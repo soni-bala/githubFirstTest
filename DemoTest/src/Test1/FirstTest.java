b7024202af5f6ce17f4a7f535d172ab7a8b977ad
soni-bala <168903162+soni-bala@users.noreply.github.com>
2024-05-04 05:45:25 -0700

    Update FirstTest.java

--- DemoTest/src/Test1/FirstTest.java
+++ DemoTest/src/Test1/FirstTest.java
@@ -5,6 +5,8 @@ public class FirstTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		System.out.println("Hi");
+		System.out.println("Good bye");
 	}
 
 }
